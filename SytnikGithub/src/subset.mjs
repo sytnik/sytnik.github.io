23312da4c6511ed05504a652337ec07cfb843471
Oleh Sytnik <oleh.sytnik@gmail.com>
2024-04-22 22:39:54 +0300

    bundling minifications added

---
+++ SytnikGithub/src/subset.mjs
@@ -0,0 +1,34 @@
+﻿import {fontawesomeSubset} from "fontawesome-subset";
+
+fontawesomeSubset(
+  {
+    brands: [
+      "telegram",
+      "linkedin",
+      "github-alt",
+      "linkedin-in"
+    ],
+    light: [
+      "stream",
+      "business-time",
+      "code",
+      "users",
+      "file-certificate",
+      "trophy",
+      "screen-users",
+      "do-not-enter",
+      "user",
+      "phone-office",
+      "at",
+      "bullseye-pointer",
+      "people-group",
+      "globe",
+      "u"
+    ]
+  },
+  "src/assets",
+  {
+    package: "pro",
+    targetFormats: ["woff2"]
+  }
+);
